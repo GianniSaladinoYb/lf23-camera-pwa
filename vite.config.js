bf7e26c5fde37906fa2069256b7637837b0483c7
gianni.saladino <gianni.saladino@youbidata.com>
2026-03-31 11:06:55 +0200

    fix: HTTPS solo in dev locale, non in produzione

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 
+const httpsConfig = fs.existsSync('.certs/key.pem')
+  ? { key: fs.readFileSync('.certs/key.pem'), cert: fs.readFileSync('.certs/cert.pem') }
+  : false
+
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: true,
-    https: {
-      key: fs.readFileSync('.certs/key.pem'),
-      cert: fs.readFileSync('.certs/cert.pem')
-    }
+    https: httpsConfig
   }
 })
